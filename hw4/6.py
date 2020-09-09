0d670953e4f9376341cba29067168dea5cf0f199
ioxnr <lionasalvia@gmail.com>
2020-09-09 16:45:31 +0300

    improved solutions

--- hw4/6.py
+++ hw4/6.py
@@ -1,18 +1,16 @@
 from itertools import count, cycle
 
 a = int(input('Введите любое целое число: '))
-new_list = []
+
 for i in count(a):
     if i > 1000:
         break
-    new_list.append(i)
-
-print(new_list)
+    print(i)
 
 b = [3, 4, 5]
 count = 0
 for i in cycle(b):
     if count > 50:
         break
-    print(i, end='')
+    print(i)
     count += 1
