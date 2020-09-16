2fccee618341f166c2168a5f74f95eca00b64af6
ioxnr <lionasalvia@gmail.com>
2020-09-16 19:56:40 +0300

    improved solutions

--- hw5/5.py
+++ hw5/5.py
@@ -2,8 +2,7 @@ with open('text_file.txt', 'w') as text_file:
     text_file.write('23 56 129 67 88 324 421 90')
 
 with open('text_file.txt') as numbers_file:
-    numbers = numbers_file.read()
-    numbers = numbers.split()
+    numbers = numbers_file.read().split()
     total = 0
     for i in numbers:
         total += int(i)
